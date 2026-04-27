c2f455df1a90fe7bb54f17285de90909c4d13d32
bsc-com-39-24 <bsc-com-39-24@unima.ac.mw>
2026-04-27 19:20:51 +0200

    Add files via upload

--- online-restaurant/src/owner/dto/create-menu.dto.ts
+++ online-restaurant/src/owner/dto/create-menu.dto.ts
@@ -1,1 +1,18 @@
+import { IsString, IsNumber, IsOptional, IsNotEmpty } from 'class-validator'; 
 
+export class CreateMenuDto { 
+@IsString() 
+@IsNotEmpty() 
+name!: string; 
+
+@IsString() 
+@IsNotEmpty() 
+description!: string; 
+
+@IsNumber() 
+price!: number; 
+
+@IsString() 
+@IsOptional() 
+availability?: string;
+}
