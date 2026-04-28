3463f2ac33a1238702674f7dfaad08f10275003b
LeilaKhonje <bed-com-34-24@unima.ac.mw>
2026-04-28 12:15:10 +0200

    Update entities to match local code

--- online-restaurant/src/orders/entities/order.entity.ts
+++ online-restaurant/src/orders/entities/order.entity.ts
@@ -1,1 +1,29 @@
-export class Order{}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { OrderItem } from './order-item.entity';
+
+@Entity()
+export class Order{
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    userId!: number;
+
+    @Column()
+    status!: string;
+
+    @Column()
+    startTime!: Date;
+
+    @Column({ unique: true})
+    qrCode!: string;
+
+    @Column({ nullable: true})
+    estimatedArrival!: Date;
+
+    @Column({nullable: true})
+    deliveredAt!: Date;
+
+    @OneToMany(() => OrderItem, item => item.order)
+    items!: OrderItem[]
+}
