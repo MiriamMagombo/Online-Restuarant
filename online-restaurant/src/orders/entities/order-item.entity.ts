842763bf87c73be44ab2b595826e9a52c88e2941
LeilaKhonje <bed-com-34-24@unima.ac.mw>
2026-04-28 12:29:27 +0200

    Update OrderItem entity

--- online-restaurant/src/orders/entities/order-item.entity.ts
+++ online-restaurant/src/orders/entities/order-item.entity.ts
@@ -1,1 +1,20 @@
-export class OrderItem{}
+import{Entity, PrimaryGeneratedColumn, Column, ManyToOne} from 'typeorm';
+import{Order} from './order.entity';
+
+@Entity()
+export class OrderItem{
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    orderId!: number;
+
+    @Column()
+    menuId!: number;
+
+    @Column()
+    quantity!: number;
+
+    @ManyToOne(()=> Order, order => order.items)
+    order!: Order;
+}
