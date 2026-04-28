c0a9b80c480e9a54a70a08db087b9c6df112d610
MiriamMagombo <Bed-com-41-24@unima.ac.mw>
2026-04-28 10:39:12 +0200

    merge new changes

--- online-restaurant/src/owner/onwer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import {MenuService} from './menu.service';
-import { MenuController } from './onwer.controller';
-
-@Module({
-    providers:[MenuService],
-    controllers: [MenuController]
-})
-export class OwnerModule {}
