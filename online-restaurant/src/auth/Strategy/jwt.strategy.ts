008d56d800fe0afbe46b82c1b178dece08d4132a
Wanangwa Nyirenda <bsc-com-40-24@unima.ac.mw>
2026-05-07 19:35:34 +0200

    updated jwt strategy

--- online-restaurant/src/auth/Strategy/jwt.strategy.ts
+++ online-restaurant/src/auth/Strategy/jwt.strategy.ts
@@ -1,17 +1,20 @@
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { PassportStrategy} from "@nestjs/passport";
-import { Injectable } from "@nestjs/common";
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy} from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
   constructor(){
     super({
-      jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
+      /*jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: process.env.JWT_SECRET,*/
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'your-secret-key',
 
     });
   }
-  async validate(...args: any) {
+  async validate(payload: any) {
       return {
         userId:payload.sub,
         username: payload.username,
@@ -20,4 +23,3 @@ export class JwtStrategy extends PassportStrategy(Strategy){
 
       };
   }
-}
